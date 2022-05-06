c8eebdfe764fd708cd1d4a8e30d3b321834e9b5f
ripal123 <pandyaripal@gmail.com>
2022-05-06 21:24:17 +0530

    attempt 3 time pass in php
    how to prevent user login 3 times fail in php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php session_start(); error_reporting (E_ALL ^ E_NOTICE); 
+if(isset($_COOKIE['user'])) { ?>
+	<meta http-equiv="refresh" content="20">
+<?php
+	echo $_COOKIE['user'];
+}
+else {
+?>
+<form action="" method="post">
+	<input type="" name="uid" placeholder="Enter UserID"><br><br>
+	<button name="btn">Click Me</button>
+</form>
+<?php
+if(isset($_POST['btn']))
+{
+	if($_POST['uid']=="avadh")
+	{
+		echo "Welcome to My Website";
+	}
+	else 	{
+			$_SESSION['u']+=1;
+			echo $_SESSION['u'];
+			if($_SESSION['u']>2)
+			{
+				header('location:time.php');
+			}
+		
+	}
+}
+}
+?>
